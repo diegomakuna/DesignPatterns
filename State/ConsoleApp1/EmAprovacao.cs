d0f7ededd3f0ac23baddc637a7a1b72e02a05d57
Diego Araujo de Oliveira <diegomakuna@gmail.com>
2021-05-02 17:47:16 -0300

    State

---
+++ State/ConsoleApp1/EmAprovacao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.State
+{
+    public class EmAprovacao : EstadoDeUmOrcamento
+    {
+        public void AplicaDescontoExtra(Orcamento orcamento)
+        {
+            orcamento.Valor = orcamento.Valor - (orcamento.Valor * 0.05);
+        }
+
+        public void Aprova(Orcamento orcamento)
+        {
+            orcamento.EstadoAtual = new Aprovado();
+        }
+
+       
+
+        public void Reprova(Orcamento orcamento)
+        {
+            orcamento.EstadoAtual = new Reprovado();
+        }
+
+        public void Finaliza(Orcamento orcamento)
+        {
+            throw new Exception("Em aprovação nao pode ir para finalizado");
+        }
+    }
+}
---
+++ State/ConsoleApp1/Orcamento.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.State
+{
+    public class Orcamento
+    {
+        public EstadoDeUmOrcamento EstadoAtual { get; set; }
+        public double Valor { get;  set; }
+        public IList<Item> Itens;
+        public Orcamento(double valor)
+        {
+            this.Valor = valor;
+            this.Itens = new List<Item>();
+            EstadoAtual = new EmAprovacao();
+        }
+
+        public void AplicaDescontoExtra(Orcamento orcamento)
+        {
+            EstadoAtual.AplicaDescontoExtra(this);
+        }
+        public void AdicionaItem(Item item)
+        {
+            Itens.Add(item);
+        }
+
+        public void Aprova()
+        {
+            EstadoAtual.Aprova(this);
+        }
+
+        public void Finaliza()
+        {
+            EstadoAtual.Finaliza(this);
+        }
+
+        public void Reprova()
+        {
+            EstadoAtual.Reprova(this);
+        }
+    }
+}
